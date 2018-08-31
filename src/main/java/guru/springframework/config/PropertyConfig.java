6c105aab949153253fe205f7175ba0ca550b1efa
Ehtiram Abdullayev <ehtiramabdullayev@gmail.com>
2018-08-31 15:58:45 +0200

    Setting additional environment properties and the use of "Environment" class

--- src/main/java/guru/springframework/config/PropertyConfig.java
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -1,11 +1,13 @@
 package guru.springframework.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import guru.springframework.examplebeans.FakeDataSource;
+import org.springframework.core.env.Environment;
 
 /**
  * @author Ehtiram_Abdullayev on 8/31/2018
@@ -24,11 +26,15 @@ public class PropertyConfig {
     @Value("${demo.dburl}")
     String url;
 
+    @Autowired
+    Environment environment;
+
 
     @Bean
     public FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(user);
+//        fakeDataSource.setUser(user);
+        fakeDataSource.setUser(environment.getProperty("demo.username"));
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
 
